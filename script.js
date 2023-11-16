1a21386241b9afceb2365385729e18b477e9af6c
Ivan <70844716+JohanPeper@users.noreply.github.com>
2023-11-16 12:01:14 +0300

    feat: add slider button action, add pop-up windows

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-const prevBtn = document.querySelector('.prev')
-const nextBtn = document.querySelector('.next')
+const loginBtn = document.querySelector('.loginBtn');
+const bgHide = document.querySelector('.bgHide');
+const registerLink = document.querySelector('.loginRegisterLink');
+const loginLink = document.querySelector('.registerLoginLink');
+
+loginBtn.onclick = () => {
+    loginLink.onclick();
+    document.querySelector('.bgHide').style.display = 'flex';
+    document.querySelector('.loginFormWrapper').style.display = 'flex';
+    document.body.style.cssText = `overflow: hidden;`;
+}
+
+bgHide.onclick = () => {
+    document.querySelector('.bgHide').style.display = 'none';
+    document.querySelector('.loginFormWrapper').style.display = 'none';
+    document.body.style.cssText = `overflow: visible;`;
+
+}
+
+registerLink.onclick = () => {
+    document.querySelector('.loginForm').style.display = 'none';
+    document.querySelector('.registerForm').style.display = 'block';
+}
+
+loginLink.onclick = () => {
+    document.querySelector('.registerForm').style.display = 'none';
+    document.querySelector('.loginForm').style.display = 'block';
+}
+
+
+const firstSlide = document.querySelector('.firstSlide')
+const secondSlide = document.querySelector('.secondSlide')
+const thirdSlide = document.querySelector('.thirdSlide')
 const outImg = document.querySelector('.sliderImg')
 
 const art1 = './images/art1.jpg'
@@ -9,14 +40,25 @@ const art3 = './images/art3.jpg'
 const images = [art1, art2, art3]
 
 
-outImg.innerHTML = `<img src=${images[0]}> <img src=${images[1]} class="middleSlide"> <img src=${images[2]}>`;
+outImg.innerHTML = `<img src=${images[0]} class='prev'> <img src=${images[1]} class="middleSlide"> <img src=${images[2]} class='next'>`;
 
-nextBtn.onclick = () => {
-    images.unshift(images.pop());
-    outImg.innerHTML = `<img src=${images[0]}> <img src=${images[1]} class="middleSlide"> <img src=${images[2]}>`;
+firstSlide.onclick = () => {
+    outImg.innerHTML = `<img src=${images[2]} class='prev'> <img src=${images[0]} class="middleSlide"> <img src=${images[1]} class='next'>`;
+    if (!firstSlide.classList.contains('activeSlide')) firstSlide.classList.toggle('activeSlide');
+    secondSlide.classList.remove('activeSlide');
+    thirdSlide.classList.remove('activeSlide');
 }
 
-prevBtn.onclick = () => {
-    images.push(images.shift());
-    outImg.innerHTML = `<img src=${images[0]}> <img src=${images[1]} class="middleSlide"> <img src=${images[2]}>`;
+secondSlide.onclick = () => {
+    outImg.innerHTML = `<img src=${images[0]} class='prev'> <img src=${images[1]} class="middleSlide"> <img src=${images[2]} class='next'>`;
+    firstSlide.classList.remove('activeSlide');
+    if (!secondSlide.classList.contains('activeSlide')) secondSlide.classList.toggle('activeSlide');
+    thirdSlide.classList.remove('activeSlide');
+}
+
+thirdSlide.onclick = () => {
+    outImg.innerHTML = `<img src=${images[1]} class='prev'> <img src=${images[2]} class="middleSlide"> <img src=${images[0]} class='next'>`;
+    firstSlide.classList.remove('activeSlide');
+    secondSlide.classList.remove('activeSlide');
+    if (!thirdSlide.classList.contains('activeSlide')) thirdSlide.classList.toggle('activeSlide');
 }
